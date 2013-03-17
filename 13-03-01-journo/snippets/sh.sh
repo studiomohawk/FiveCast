ae964e316fd3aabfed6371eed1e24b4718427b9b
Yuya Saito <studiomohawk@gmail.com>
2013-03-17 21:20:14 +0900

    update gitignore

---
+++ 13-03-01-journo/snippets/sh.sh
@@ -0,0 +1,4 @@
+journo init    #ディレクトリ内にブログを生成する
+journo build   #siteディレクトリ内に静的ファイルを生成する
+journo preview #ローカルサーバ内でブログをプレビューする
+journo publish #リモートサーバにブログをアップロードする
---
+++ readme.md
@@ -0,0 +1,3 @@
+# FiveCast
+
+FiveCast is a mini(supposably within five mins) screencast series.
